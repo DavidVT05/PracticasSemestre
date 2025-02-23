0263fcdc629ccccd290f2275b357ab4790d42a74
DAVIDVT05 <al23760344@ite.edu.mx>
2025-02-22 20:49:35 -0800

    creacion de bibliotecas de boton y texbox

---
+++ BotonLibreria/Boton1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BotonLibreria
+{
+    public partial class Boton1: UserControl
+    {
+        private Color colorOriginal;
+        public Boton1()
+        {
+            InitializeComponent();
+            colorOriginal = this.BackColor;
+            this.MouseEnter += (s, e) => this.BackColor = Color.DarkBlue;
+            this.MouseLeave += (s, e) => this.BackColor = colorOriginal;
+            this.DoubleClick += btnAgregar_Click;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Daras click, estas seguro?", "Aceptar", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ CuadroTexto/CuadroText.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuadroTexto
+{
+    public partial class CuadroText: UserControl
+    {
+        public enum InputType { Numeros, Letras, Ambos }
+        public InputType ValorEntrada { get; set; } = InputType.Ambos;
+        public CuadroText()
+        {
+            InitializeComponent();
+        }
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+            if (ValorEntrada == InputType.Numeros && !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+                this.BackColor = Color.Coral;
+            }
+            else if (ValorEntrada == InputType.Letras && !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+                this.BackColor = Color.Bisque;
+            }
+            else
+            {
+                this.BackColor = Color.Black;
+            }
+        }
+    }
+}
